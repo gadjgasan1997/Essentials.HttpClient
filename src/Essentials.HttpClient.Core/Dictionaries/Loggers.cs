a6260765d8573dc573e0ef1269e9feb6b0a7c5c4
gadjgasan1997 <gadj.gasan1997@gmail.com>
2023-07-30 21:41:22 +0300

    Добавил билдер построения запросов

---
+++ src/Essentials.HttpClient.Core/Dictionaries/Loggers.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace Essentials.HttpClient.Dictionaries;
+
+/// <summary>
+/// Известные логгеры
+/// </summary>
+public static class Loggers
+{
+    /// <summary>
+    /// Основной логгер
+    /// </summary>
+    public static Logger MainLogger { get; } = LogManager.GetLogger("Essentials.HttpClient.Main");
+}
