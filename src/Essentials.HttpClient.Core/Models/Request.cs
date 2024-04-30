e76c0129ff5dd8b294df147311c9320195ac1967
gadjgasan1997 <gadj.gasan1997@gmail.com>
2024-04-30 23:17:36 +0300

    Fix модели запроса и ответа

--- src/Essentials.HttpClient.Core/Models/Request.cs
+++ src/Essentials.HttpClient.Core/Models/Request.cs
@@ -1,52 +1,87 @@
 ﻿using LanguageExt;
 using System.Net.Http.Headers;
+using System.Text;
 using Essentials.Utils.Extensions;
 using Essentials.HttpClient.Events;
 
 namespace Essentials.HttpClient.Models;
 
 /// <inheritdoc cref="IRequest" />
-internal record Request(
-    string Id,
-    string ClientName,
-    Option<string> TypeId,
-    Uri Uri,
-    Option<MediaTypeHeaderValue> MediaType,
-    Option<TimeSpan> Timeout,
-    Option<RequestMetricsOptions> MetricsOptions,
-    IEnumerable<Action<HttpRequestMessage>>? ModifyRequestActions = null,
-    IEnumerable<Type>? Interceptors = null,
-    Dictionary<string, Handler>? EventsHandlers = null) : IRequest
+internal record Request : IRequest
 {
+    public Request(
+        string id,
+        string clientName,
+        Option<string> typeId,
+        Uri uri,
+        Option<MediaTypeHeaderValue> mediaType,
+        Option<TimeSpan> timeout,
+        Option<RequestMetricsOptions> metricsOptions,
+        IEnumerable<Action<HttpRequestMessage>>? modifyRequestActions = null,
+        IEnumerable<Type>? interceptors = null,
+        Dictionary<string, Handler>? eventsHandlers = null)
+    {
+        Id = id.CheckNotNullOrEmpty("Id запроса не может быть пустым");
+        ClientName = clientName.CheckNotNullOrEmpty("Название клиента не может быть пустым");
+        TypeId = typeId;
+        Uri = uri.CheckNotNull("Uri запроса не может быть пустым");
+        MediaType = mediaType;
+        Timeout = timeout;
+        MetricsOptions = metricsOptions;
+        ModifyRequestActions = modifyRequestActions ?? Array.Empty<Action<HttpRequestMessage>>();
+        Interceptors = interceptors ?? Array.Empty<Type>();
+        EventsHandlers = eventsHandlers ?? new Dictionary<string, Handler>();
+    }
+    
     /// <inheritdoc cref="IRequest.Id" />
-    public string Id { get; } = Id.CheckNotNullOrEmpty("Id запроса не может быть пустым");
+    public string Id { get; }
 
     /// <inheritdoc cref="IRequest.ClientName" />
-    public string ClientName { get; } = ClientName.CheckNotNullOrEmpty("Название клиента не может быть пустым");
+    public string ClientName { get; }
 
     /// <inheritdoc cref="IRequest.TypeId" />
-    public Option<string> TypeId { get; } = TypeId;
+    public Option<string> TypeId { get; }
 
     /// <inheritdoc cref="IRequest.Uri" />
-    public Uri Uri { get; } = Uri.CheckNotNull("Uri запроса не может быть пустым");
+    public Uri Uri { get; }
 
     /// <inheritdoc cref="IRequest.MediaType" />
-    public Option<MediaTypeHeaderValue> MediaType { get; set; } = MediaType;
+    public Option<MediaTypeHeaderValue> MediaType { get; set; }
 
     /// <inheritdoc cref="IRequest.Timeout" />
-    public Option<TimeSpan> Timeout { get; } = Timeout;
+    public Option<TimeSpan> Timeout { get; }
 
     /// <inheritdoc cref="IRequest.MetricsOptions" />
-    public Option<RequestMetricsOptions> MetricsOptions { get; } = MetricsOptions;
+    public Option<RequestMetricsOptions> MetricsOptions { get; }
 
     /// <inheritdoc cref="IRequest.ModifyRequestActions" />
-    public IEnumerable<Action<HttpRequestMessage>> ModifyRequestActions { get; } =
-        ModifyRequestActions ?? Array.Empty<Action<HttpRequestMessage>>();
+    public IEnumerable<Action<HttpRequestMessage>> ModifyRequestActions { get; }
 
     /// <inheritdoc cref="IRequest.Interceptors" />
-    public IEnumerable<Type> Interceptors { get; } = Interceptors ?? Array.Empty<Type>();
+    public IEnumerable<Type> Interceptors { get; }
 
     /// <inheritdoc cref="IRequest.EventsHandlers" />
-    public Dictionary<string, Handler> EventsHandlers { get; } =
-        EventsHandlers ?? new Dictionary<string, Handler>();
+    public Dictionary<string, Handler> EventsHandlers { get; }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+
+        builder
+            .Append("{ ")
+            .Append("\"Id запроса\": \"")
+            .Append(Id)
+            .Append("\", ")
+            .Append("\"Название http клиента\": \"")
+            .Append(ClientName)
+            .Append("\", ")
+            .Append("\"Тип запроса\": \"")
+            .Append(TypeId)
+            .Append("\", ")
+            .Append("\"Адрес запроса\": \"")
+            .Append(Uri)
+            .Append("\" }");
+        
+        return builder.ToString();
+    }
 }
--- src/Essentials.HttpClient.Core/Models/Response.cs
+++ src/Essentials.HttpClient.Core/Models/Response.cs
@@ -1,13 +1,39 @@
-﻿using Essentials.Utils.Extensions;
+﻿using System.Text;
+using Essentials.Utils.Extensions;
 
 namespace Essentials.HttpClient.Models;
 
 /// <inheritdoc cref="IResponse" />
-internal record Response(IRequest Request, HttpResponseMessage ResponseMessage) : IResponse
+internal record Response : IResponse
 {
+    public Response(IRequest request, HttpResponseMessage responseMessage)
+    {
+        Request = request.CheckNotNull();
+        ResponseMessage = responseMessage.CheckNotNull();
+    }
+    
     /// <inheritdoc cref="IResponse.Request" />
-    public IRequest Request { get; } = Request;
+    public IRequest Request { get; }
     
     /// <inheritdoc cref="IResponse.ResponseMessage" />
-    public HttpResponseMessage ResponseMessage { get; } = ResponseMessage.CheckNotNull();
+    public HttpResponseMessage ResponseMessage { get; }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+
+        builder
+            .Append("{ ")
+            .Append("\"Полученный ответ\": ")
+            .Append("{ ")
+            .Append("\"Id запроса\": \"")
+            .Append(Request.Id)
+            .Append("\", ")
+            .Append("\"Код ответа\": \"")
+            .Append(ResponseMessage.StatusCode)
+            .Append("\" }")
+            .Append(" }");
+        
+        return builder.ToString();
+    }
 }
